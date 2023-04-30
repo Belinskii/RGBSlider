71d63ca6444f41b50fbe82906d23b496fe471d0b
Vladislav <Belinskii>
2023-04-30 10:57:26 +0300

    connection

--- RGBSlider/ViewController.swift
+++ RGBSlider/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    //MARK: - IBOutlets
+    @IBOutlet var redArea: UIView!
+    @IBOutlet var greenArea: UIView!
+    @IBOutlet var blueArea: UIView!
+    
+    @IBOutlet var redNumber: UILabel!
+    @IBOutlet var greenNumber: UILabel!
+    @IBOutlet var blueNumber: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    private let rad: CGFloat = 20
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redArea.layer.cornerRadius = rad
+        greenArea.layer.cornerRadius = rad
+        blueArea.layer.cornerRadius = rad
     }
-
+    
+    //MARK: - IBActions
+    @IBAction func redSliderAction() {
+    }
+    @IBAction func greenSliderAction() {
+    }
+    @IBAction func blueSliderAction() {
+    }
+    
 
 }
 
