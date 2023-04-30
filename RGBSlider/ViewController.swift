96f66ce0743f3c01711af9f3dff770b22008c0b8
Vladislav <Belinskii>
2023-04-30 15:05:59 +0300

    It works

--- RGBSlider/ViewController.swift
+++ RGBSlider/ViewController.swift
@@ -11,8 +11,6 @@ final class ViewController: UIViewController {
     
     //MARK: - IBOutlets
     @IBOutlet var redArea: UIView!
-    @IBOutlet var greenArea: UIView!
-    @IBOutlet var blueArea: UIView!
     
     @IBOutlet var redNumber: UILabel!
     @IBOutlet var greenNumber: UILabel!
@@ -22,23 +20,33 @@ final class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    private let rad: CGFloat = 20
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redArea.layer.cornerRadius = rad
-        greenArea.layer.cornerRadius = rad
-        blueArea.layer.cornerRadius = rad
+        redArea.layer.cornerRadius = CGFloat(20)
+        
+        redNumber.text = String(format: "%.2f", redSlider.value)
+        greenNumber.text = String(format: "%.2f", greenSlider.value)
+        blueNumber.text = String(format: "%.2f", blueSlider.value)
     }
     
     //MARK: - IBActions
     @IBAction func redSliderAction() {
+        redNumber.text = String(format: "%.2f", redSlider.value)
+        redArea.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
+    
     @IBAction func greenSliderAction() {
+        greenNumber.text = String(format: "%.2f", greenSlider.value)
+        redArea.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
+    
     @IBAction func blueSliderAction() {
+        blueNumber.text = String(format: "%.2f", blueSlider.value)
+        redArea.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
+    
+    //MARK: - Private Methods
     
 
 }
